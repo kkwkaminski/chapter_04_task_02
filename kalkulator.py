d9be9b0b11ea698285059b6e1917a0f290312e79
Kamil Laptop <kkw.kaminski@gmail.com>
2023-12-18 20:54:07 +0100

    Zmiana loggingów na printy wg zalecenia

--- kalkulator.py
+++ kalkulator.py
@@ -1,5 +1,3 @@
-import logging
-
 def dodawanie(*args):
     return sum(args)
 
@@ -18,28 +16,22 @@ def dzielenie(a, b):
     else:
         return "Nie można dzielić przez zero!"
 
-
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-
     print("Podaj działanie, posługując się odpowiednią liczbą:")
     print("1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-    str_operation= ['Dodaję:', 'Odejmuję:','Mnożę:', 'Dzielę:']
+    str_operation = ['Dodaję:', 'Odejmuję:', 'Mnożę:', 'Dzielę:']
     operation = int(input())
-    
+
     if operation in [1, 2, 3, 4]:
-        logging.info("Wybrano operację: %s", operation)
-        
+        print(f"Wybrano operację: {operation}")
+
         if operation in [1, 3]:  # Dodawanie lub mnożenie
-            logging.info("Podaj składniki oddzielone spacją:")
-            components = [float(x) for x in input().split()]
-            logging.info(f"{str_operation[operation-1]} {', '.join(map(str, components)) if len(components) > 1 else components[0]}")
+            components = [float(x) for x in input("Podaj składniki oddzielone spacją: ").split()]
+            print(f"{str_operation[operation-1]} {', '.join(map(str, components)) if len(components) > 1 else components[0]}")
         else:  # Odejmowanie lub dzielenie
-            logging.info("Podaj składnik 1:")
-            component1 = float(input())
-            logging.info("Podaj składnik 2:")
-            component2 = float(input())
-            logging.info(f"{str_operation[operation-1]} {component1} i {component2}")
+            component1 = float(input("Podaj składnik 1: "))
+            component2 = float(input("Podaj składnik 2: "))
+            print(f"{str_operation[operation-1]} {component1} i {component2}")
 
         if operation == 1:
             result = dodawanie(*components)
